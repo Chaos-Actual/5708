3330da8727aa772565e6f18d9a67163c2114514e
Mark McGuire <mcgu0156@umn.edu>
2018-04-02 17:27:31 -0500

    completed write-up parts a - g

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -20,7 +20,10 @@ SELECT VOTES FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 SELECT VOTES FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 --output 66825
 
-
+/*The update transaction was still active and had not committed. The record was updated in memory but had not flushed to disk yet.
+The record in memory can be accesses and will show the updated result. When the transaction is terminated then the update is lost. 
+When the system comes back up, after logging back in, the record is pulled from disk which has the value prior to the update. 
+*/
 
 
 --Part B 
@@ -38,6 +41,11 @@ SAVEPOINT S0;
 --5 
 SELECT VOTES FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 -- output 66825
+/*
+The update transaction was still active and had not committed. Again, the record was updated in memory and not flushed to disk. 
+The savepoint was set but is deleted when a transaction terminates. Savepoints are only used to rollback the transaction that created the savepoint. 
+A savepoint is a point in the transaction that can be referenced using a rollback.  
+*/
 
 
 --Part C
@@ -97,7 +105,7 @@ ROLLBACK TO S1;
 
 
 
--- part D
+-- Part D
 --1 Both logged into SQL
 
 --2
@@ -125,13 +133,13 @@ SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 --8
 UPDATE MOVIE SET SCORE = 10 WHERE TITLE = 'My Cousin Vinny';
 -- My partner gets 7.5 while I get 10. for step 6 and 7.
--- Step 8 When my partner trys to update the score from My Cousin Vinny it hangs. 
---This is becuase I have a lock on the record and his process cannot update until I release the lock.
+-- Step 8 When my partner tries to update the score from My Cousin Vinny it hangs. 
+--This is because I have a lock on the record and his process cannot update until I release the lock.
 
 --9
 SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 --output 7.5 
--- My partner  cancels then requries and still sees 7.5. This happens because I have the lock and my partner is reading the value before I wrote to the record.
+-- My partner cancels then requires and still sees 7.5. This happens because I have the lock and my partner is reading the value before I wrote to the record.
 
 --10
 commit;
@@ -142,8 +150,8 @@ SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 -- I commit then he sees the correct score of 10. 
 
 --12
---Oracle uses a 2 phase locking schemes. My process aquires a write lock and does not allow my partner to see my updates until I commit.
--- When I commit the all of the locks that I had are relased and other trasactions can see my changes. 
+--Oracle uses a 2 phase locking schemes. My process acquires a write lock and does not allow my partner to see my updates until I commit.
+-- When I commit the all of the locks that I had are released and other transactions can see my changes. 
 
 --13
 Set AUTOCOMMIT OFF;
@@ -178,7 +186,7 @@ SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 
 --8s
 UPDATE MOVIE SET SCORE = (SELECT MAX(SCORE ) FROM MOVIE) WHERE TITLE = 'My Cousin Vinny'; 
--- Again this process hangs. Same result as before. 
+-- Again, this process hangs. Same result as before. 
 
 --9s
 SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
@@ -193,94 +201,105 @@ SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
 -- This is different result from before. 
 -- 17 and 18
 --After I commit my partner still sees the score as 7.5. 
---This is because the serializable trasaction level my partner's transaction will read the score as 7.5 until his trasaction commits.
--- I can update the score of My Cousin Vinny as much as i want be he will not be able to see any other score.
--- Read commited snapshot reads the score of the last commited transation. This is the current scheme being used. 
--- Once my partner commits his trasaction then he will be able to see the updated score.
+--This is because the serializable transaction level my partner's transaction will read the score as 7.5 until his transaction commits.
+-- I can update the score of My Cousin Vinny as much as I want but he will not be able to see any other score.
+-- Read committed snapshot reads the score of the last committed transaction. This is the current scheme being used. 
+-- Once my partner commits his transaction then he will be able to see the updated score.
 
 
 
+--Part E
+--1 
+Set AUTOCOMMIT OFF;
 
+--2 Drop all tables and reload tables and commit;
 
+--3 
+Set AUTOCOMMIT OFF;
 
+--4
+SELECT SCORE FROM MOVIE WHERE TITLE = 'My Cousin Vinny';
+--output 7.5 
 
+--5
+UPDATE MOVIE SET SCORE = 10 WHERE TITLE = 'My Cousin Vinny';
+--output 1 row updated.  My partner is allowed to update the score of My Cousin Vinny;
 
+--6 Partner commits
+COMMIT:
 
+--7
+SELECT * FROM MOVIE WEHRE TITLE = 'My Cousin Vinny' FOR UPDATE OF SCORE NOWAIT;
+-- output all columns for movie titled 'My Cousin Vinny'
 
-
-
-
-
-
-
-
-update movie set score = (select max(score) from movie) where title = 'My Cousin Vinny';
-
-
-
-
---part e
-step 5: My partner is allowed to update the score of My Cousin Vinny;
-
+--8 
+UPDATE MOIVE SET SCORE = 7.5 WHERE TITLE = 'My Cousin Vinny';
 
 SELECT * FROM movie WHERE title = 'My Cousin Vinny' FOR UPDATE OF score NOWAIT;
+--output the update hangs. 
+--My partner is not allowed to update the score. The FOR UPDATE locks records that are being used for the query. 
+--The nowait clause tells oracle not to wait if the record has been locked by another transaction.
 
---step 8 My partner is not allowed to update the score. THe FOR UPDATE locks records that are being used for the query. the nowait clause tells oracle not to wait if the recored has been locked by another trasaction.
+--9
+ROLLBACK;
+--output ROLLBACK COMPLETE
 
-step 11;
-I receive and error ORA-0054;resource busy and acquire with NOWAIT specified or timeout expired. This is expected becuase The nowaite clause is set so my trasaction will not wait for resources that are locked by another trasaction.
+--10
+LOCK TABLE MOVIE IN EXCLUSIVE MODE;
+--output TABLE(S) LOCKED
+
+--11
+UPDATE MOVIE SET SCORE = 10 WHERE TITLE = 'My Cousin Vinny';
+--output The update hangs.
+-- This is expected because my partner has an exclusive lock on the table movie. My transaction must wait until my partner releases the lock on the movie table. 
+
 
 --part F:
+--1
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
+--2 
+SET AUTOCOMMIT OFF;
+
+--3
+SELECT TITLE FROM MOIVE WHERE TITLE LIKE 'K%';
+--output 164 rows selected
+
+--4 
+SELECT TITLE FROM MOVIE WHERE TITLE LIKE 'K%';
+--output 164 rows selected 
+
+--5 
+DROP TABLE CASTING;
+--output table dropped
+DELETE FROM MOVIE WHERE TITLE LIKE 'K%';
+--output 164 rows deleted 
 INSERT INTO movie(id, title, yr, score, votes, director) VALUES(2632454,'Kingsman: The Secret Service', 2014, 8.2, 84535, 'Matthew Vaughn');
+--output 1 row created 
 
-step 7; My partner saw the inserted row only. This was different from his first select where he saw all 164 movies that started with K. This is a non-repeatble read at the default isolation level.
-Phantom reads can also occur at this level because i can be inserting and/or updating a recored while my partner is querying these records. This can be prevented in oracle by changing the isolation level to serializable.
+--6
+COMMIT;
+
+--7
+SELECT TITLE FROM MOVIE WHERE TITLE LIKE 'K%';
+--output 'Kingsman: The Secret Service'
+
+--My partner saw the inserted row only. This was different from his first select where he saw all 164 movies that started with K. 
+--This is a non-repeatable read at the default isolation level.
+--Phantom reads can also occur at this level because I can be inserting and/or updating a record while my partner is querying these same records.
+-- This can be prevented in oracle by changing the isolation level to serializable.
 
 
 --Part G:
 
 1: Oracles CCT is not conservative;
 2. oracle cct is strict;
-3. Oracle obays 2-phase locking.
-4. Oracle's cct can lead to phatom problems in the default isolation level. We saw this in step 7 of part F.
-5. Oracle's default cct can lead to deadlocks because the database does not employ a conservative 2-phase locking stratigy.
+3. Oracle obeys 2-phase locking.
+4. Oracle's cct can lead to phantom problems in the default isolation level. We saw this in step 7 of part F.
+5. Oracle's default cct can lead to deadlocks because the database does not employ a conservative 2-phase locking strategy.
 6. Oracle's CCT cannot lead to a livelock.
-7. Oracle's cct default isolation level read committed does not allow dirty reads this means that it is not possible for a cascading rollback. It is possible to set the isolation level to read uncommmitted which does allow for dirty reads. In this case it would be possible for oracle to have a cascading rollback.
-8. Oracle's cct dedauld isolation level read committed is recoverable. It is possible to set the isolation level to read uncommitted which would allow,dirty reads, and consequently allow schedules to become unrecoverable. 
+7. Oracle's cct default isolation level read committed does not allow dirty reads this means that it is not possible for a cascading rollback. It is possible to set the isolation level to read uncommitted which does allow for dirty reads. In this case it would be possible for oracle to have a cascading rollback.
+8. Oracle's cct default isolation level read committed is recoverable. It is possible to set the isolation level to read uncommitted which would allow, dirty reads, and consequently allow schedules to become unrecoverable. 
 
 
 
-
-
-
-
-
-
-
-
-
-part D
-Ask your partner to re-query the score of ' 'My Cousin Vinny' '. Does your partner see the change by your update ?
-Recall that 2 phase locking schemes with read/write locks. Does Oracle follow the 2 phase locking schemes? Which kind of locking does Oracle follow? Explain your observation. 
-
-Next, we examine the effect of a change of transaction isolation level. 
-
-You and your partner re-login to SQL*Plus and ensure that AUTOCOMMIT is OFF.
-
---step 9 he cancels then requries and still sees 7.5 he can read but I have not commited the changes yet.
--- I commit then he sees the correct score of 10.
---IT releases the locks once the original process commit. When a process commits then it will release all of the locks that it has obtained.
-
-step 16;
-after I commit my partner still sees the score as 7.5; This is because the serializable trasaction level my partner's transaction will read the score as 7.5 until his trasaction commits. I can update the score of My Cousin Vinny as much as i want be he will not be able to see any other score.
-read commited snapshot reads the score of the last commited transation.  once my partner commits his trasaction then he will be able to see the updated score.
-
-
-
-
-
-
-
-
- releases the locks once the original process commit. When a process commits then it will release all of the locks that it has obtained.
